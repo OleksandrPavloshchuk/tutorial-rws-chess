1ac28ffd60548a6d533a7bbd363f921bd7779f52
Oleksandr Pavloshchuk <ap730518@gmail.com>
2019-01-14 16:45:53 +0200

    1st server web sockets implementation

--- mediator/main.go
+++ mediator/main.go
@@ -1,31 +1,115 @@
 package main
 
 import (
-  "os"
-  "fmt"
+  "io"
+  "log"
+  "net/http"
+  "encoding/json"
+  "github.com/gorilla/websocket"
   "./players"
 )
 
-func temp_login(name string, password string) {
-  if m := players.Login(name, password); m != nil {
-    fmt.Fprintf(os.Stderr, "LOGIN: %s\n", m)
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func login(msg []byte) error {
+  var player players.Player
+  if err := json.Unmarshal( msg, &player ); err != nil {
+    return err
+  } else {
+    return players.Login(player)
   }
 }
+
+func registerAbout() {
+  http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+    w.WriteHeader(http.StatusOK)
+    io.WriteString(w, ABOUT)
+  })
+}
+
+func registerLogin() {
+  http.HandleFunc("/login", func (w http.ResponseWriter, r *http.Request) {
+    if conn, err := upgrader.Upgrade(w, r, nil); err != nil {
+      log.Printf("LOGIN error: %v", err)
+    } else {
+      // Read message from browser
+      if msgType, msg, err := conn.ReadMessage(); err != nil {
+        log.Printf("LOGIN error: %v", err)
+      } else {
+        s := ""
+        err := login(msg)
+        if err != nil {
+          s = err.Error()
+        }
+        if err = conn.WriteMessage(msgType, []byte(s)); err != nil {
+          log.Printf("LOGIN error: %v", err)
+        }
+      }
+    }
+    // TODO send waiting player list for every participant
+  })
+}
+
+func registerLogout() {
+  http.HandleFunc("/logout", func (w http.ResponseWriter, r *http.Request) {
+    if conn, err := upgrader.Upgrade(w, r, nil); err != nil {
+      log.Printf("LOGOUT error: %v", err)
+    } else {
+      // Read message from browser
+      if _, msg, err := conn.ReadMessage(); err != nil {
+        log.Printf("LOGOUT error: %v", err)
+      } else {
+        players.Logout(string(msg))
+        // TODO send waiting player list for every participant
+      }
+    }
+  })
+}
+
+func registerPlayerList() {
+  http.HandleFunc("/players", func (w http.ResponseWriter, r *http.Request) {
+    if conn, err := upgrader.Upgrade(w, r, nil); err != nil {
+      log.Printf("PLAYER LIST error: %v", err)
+    } else {
+      // Read message from browser
+      if msgType, msg, err := conn.ReadMessage(); err != nil {
+        log.Printf("PLAYER LIST error: %v", err)
+      } else {
+        list := players.RetrieveWaitingPlayers(string(msg))
+        data, err := json.Marshal(list)
+        if err != nil {
+          log.Printf("PLAYER LIST error: %v", err)
+        } else {
+          if err = conn.WriteMessage(msgType, data); err != nil {
+            log.Printf("PLAYER LIST error: %v", err)
+          }
+        }
+      }
+    }
+  })
+}
+
 
 func main() {
   players.Init()
 
-  temp_login("player-1", "bebebe")
-  temp_login("player-1", "123456")
-  temp_login("wrong", "---")
-  temp_login("player-2", "654321")
-  temp_login("me", "1")
-  temp_login("player-2", "654321")
+  registerAbout()
+  registerLogin()
+  registerLogout()
+  registerPlayerList()
 
-  fmt.Printf("active users = %v\n", players.RetrieveWaitingPlayers("me"))
-
-  players.Logout("player-1")
-
-  fmt.Printf("active users = %v\n", players.RetrieveWaitingPlayers("player-2"))  
-
+  http.ListenAndServe(":3016", nil)
 }
+
+const ABOUT = `
+Chess React.js + Web Sockets + Go mediator.
+
+Usage:
+  /login - check credential and Login
+  /logout - logout from game
+  /players - list of waiting players (without me)
+`
--- mediator/players/players.go
+++ mediator/players/players.go
@@ -14,35 +14,35 @@ const (
 var activePlayers map[string]mode
 
 // TODO (2019/01/14) check credentials from database
-type player struct {
+type Player struct {
   name string
   password string
 }
-var registeredPlayers [3]player
+var registeredPlayers [3]Player
 
 func Init() {
   activePlayers = make(map[string]mode, 0)
-  registeredPlayers[0] = player { name:"player-1", password: "123456"}
-  registeredPlayers[1] = player { name:"player-2", password: "654321"}
-  registeredPlayers[2] = player { name:"me", password: "1"}
+  registeredPlayers[0] = Player { name:"player-1", password: "123456"}
+  registeredPlayers[1] = Player { name:"player-2", password: "654321"}
+  registeredPlayers[2] = Player { name:"me", password: "1"}
 }
 
-func Login(name string, password string) error {
+func Login(p Player) error {
 
   // Is this player active?
-  _, active := activePlayers[name]
+  _, active := activePlayers[p.name]
   if active {
     return errors.New("This player is already logged in")
   }
 
   // Check the password and register
-  for _, p := range registeredPlayers {
-    if p.name == name && p.password == password {
-      activePlayers[name] = waiting
+  for _, f := range registeredPlayers {
+    if f.name == p.name && f.password == p.password {
+      activePlayers[p.name] = waiting
       return nil
     }
   }
-
+  
   return errors.New("Login failed")
 }
 
