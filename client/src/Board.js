32d603bf19af4a791144aee618faa8b7c5b2ed7c
Oleksandr Pavloshchuk <ap730518@gmail.com>
2019-01-18 19:16:58 +0200

    new board generation

--- client/src/Board.js
+++ client/src/Board.js
@@ -6,111 +6,97 @@ export default class Board extends Component {
 
   render() {
 
-    function renderWhiteHeader(key) {
-      return <tr key={key}>
-              <td className="square-label"></td>
-              <td className="square-label">a</td>
-              <td className="square-label">b</td>
-              <td className="square-label">c</td>
-              <td className="square-label">d</td>
-              <td className="square-label">e</td>
-              <td className="square-label">f</td>
-              <td className="square-label">g</td>
-              <td className="square-label">h</td>
-              <td className="square-label"></td>
-             </tr>;
-    }
-
-    function renderBlackHeader(key) {
-      return <tr key={key}>
-              <td className="square-label"></td>
-              <td className="square-label">h</td>
-              <td className="square-label">g</td>
-              <td className="square-label">f</td>
-              <td className="square-label">e</td>
-              <td className="square-label">d</td>
-              <td className="square-label">c</td>
-              <td className="square-label">b</td>
-              <td className="square-label">a</td>
-              <td className="square-label"></td>
-             </tr>;
-    }
-
-    function renderRowBlackFirst(i) {
-      return <tr key={'row' + i}>
-              <td className="square-label">{i}</td>
-              <td className="black-square"></td>
-              <td className="white-square"></td>
-              <td className="black-square"></td>
-              <td className="white-square"></td>
-              <td className="black-square"></td>
-              <td className="white-square"></td>
-              <td className="black-square"></td>
-              <td className="white-square"></td>
-              <td className="square-label">{i}</td>
-             </tr>;
-    }
-
-    function renderRowWhiteFirst(i) {
-      return <tr key={'row' + i}>
-              <td className="square-label">{i}</td>
-              <td className="white-square"></td>
-              <td className="black-square"></td>
-              <td className="white-square"></td>
-              <td className="black-square"></td>
-              <td className="white-square"></td>
-              <td className="black-square"></td>
-              <td className="white-square"></td>
-              <td className="black-square"></td>
-              <td className="square-label">{i}</td>
-             </tr>;
-    }
-
-
     let squares = [];
-    if( this.props.app.state.whiteMe ) {
-      squares.push( renderWhiteHeader("htop") );
-      squares.push( renderRowBlackFirst(8) );
-      squares.push( renderRowWhiteFirst(7) );
-      squares.push( renderRowBlackFirst(6) );
-      squares.push( renderRowWhiteFirst(5) );
-      squares.push( renderRowBlackFirst(4) );
-      squares.push( renderRowWhiteFirst(3) );
-      squares.push( renderRowBlackFirst(2) );
-      squares.push( renderRowWhiteFirst(1) );
-      squares.push( renderWhiteHeader("hbottom") );
-    } else {
-      squares.push( renderBlackHeader("htop") );
-      squares.push( renderRowBlackFirst(1) );
-      squares.push( renderRowWhiteFirst(2) );
-      squares.push( renderRowBlackFirst(3) );
-      squares.push( renderRowWhiteFirst(4) );
-      squares.push( renderRowBlackFirst(5) );
-      squares.push( renderRowWhiteFirst(6) );
-      squares.push( renderRowBlackFirst(7) );
-      squares.push( renderRowWhiteFirst(8) );
-      squares.push( renderBlackHeader("hbottom") );
+
+    let labels = ['', 'a','b','c','d','e','f','g','h', ' '];
+    if( !this.props.app.state.whiteMe ) {
+        labels = labels.reverse();
     }
+
+    squares.push(<LRow key="top" labels={labels}/>);
+    for( var i=1; i<=8; i++ ) {
+        const l = this.props.app.state.whiteMe ? 9 - i : i;
+        const r = i % 2 === 0
+          ? <Row label={l} key={i} type={0} />
+          : <Row label={l} key={i} type={1} />
+        squares.push(r);
+    }
+    squares.push(<LRow key="bottom" labels={labels}/>);
 
     return (
         <div className="card board">
           <div className="card-body">
-            {this.props.app.state.whiteMe &&
-            <table><tbody>
-            <tr><td colSpan="10"><h5 className="card-title text-center">{this.props.app.state.otherPlayer}</h5></td></tr>
+            <table>
+            <tbody>
+            <Player name={this.props.app.state.otherPlayer} />
             {squares}
-            <tr><td colSpan="10"><h5 className="card-title text-center">{this.props.app.state.player}</h5></td></tr>
-            </tbody></table>
-            }
-            {!this.props.app.state.whiteMe &&
-            <table><tbody>
-            <tr><td colSpan="10"><h5 className="card-title text-center">{this.props.app.state.otherPlayer}</h5></td></tr>
-            {squares}
-            <tr><td colSpan="10"><h5 className="card-title text-center">{this.props.app.state.player}</h5></td></tr>
-            </tbody></table>
-            }
+            <Player name={this.props.app.state.player} />
+            </tbody>
+            </table>
           </div>
         </div>
+    );
+  }
+}
+
+class Player extends Component {
+  render() {
+    return (
+      <tr><td colSpan="10"><h5 className="card-title text-center">{this.props.name}</h5></td></tr>
+    );
+  }
+}
+
+class LRow extends Component {
+  render() {
+    return (
+      <tr key={this.props.key}>
+        {this.props.labels.map( l => <LCell text={l} key={l}/>)}
+      </tr>
+    );
+  }
+}
+
+class Row extends Component {
+  render() {
+    let cs = [];
+    for( var i=1; i<=8; i++ ) {
+      const c = i % 2 === this.props.type
+        ? <BCell key={'c' + this.props.label + i} />
+        : <WCell key={'c' + this.props.label + i} />
+      cs.push(c);
+    }
+
+    return (
+      <tr key={this.props.key}>
+        <LCell text={this.props.label} />
+        {cs}
+        <LCell text={this.props.label} />
+      </tr>
+    );
+  }
+}
+
+class LCell extends Component {
+  render() {
+    return (
+      <td className="cell-label" key={this.props.key}>{this.props.text}</td>
+    );
+  }
+}
+
+class WCell extends Component {
+  render() {
+    return (
+      <td className="cell-white" key={this.props.key}></td>
+    );
+  }
+}
+
+class BCell extends Component {
+  render() {
+    return (
+      <td className="cell-black" key={this.props.key}></td>
     );
   }
 }
