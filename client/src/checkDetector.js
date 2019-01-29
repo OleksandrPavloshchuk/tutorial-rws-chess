1b0247103c742004c2b61bdd2ba0b7a5b1f874bc
OleksandrPavloshchuk <ap730518@gmail.com>
2019-01-29 21:52:11 +0200

    1st variant of check detector

---
+++ client/src/checkDetector.js
@@ -0,0 +1,74 @@
+import {key, x, y} from './boardData';
+
+export default class CheckDetector {
+
+  constructor(src, board) {
+    this.src = src;
+    this.x = x(src);
+    this.y = y(src);
+    this.board = board;
+    this.piece = this.board.get(this.src);
+
+    this.checkDetectors = {
+      pawn : () => {
+        if(this.piece.white) {                    
+          return this.checkCell(this.x-1, this.y+1) || this.checkCell(this.x+1, this.y+1);
+        } else {
+          return this.checkCell(this.x-1, this.y-1) || this.checkCell(this.x+1, this.y-1);
+        }
+      },
+      knight : () => this.checkCell(this.x+1, this.y+2)
+          || this.checkCell(this.x+2, this.y+1)
+          || this.checkCell(this.x-1, this.y-2)
+          || this.checkCell(this.x-2, this.y-1)
+          || this.checkCell(this.x+1, this.y-2)
+          || this.checkCell(this.x-2, this.y+1)
+          || this.checkCell(this.x-1, this.y+2)
+          || this.checkCell(this.x+2, this.y-1),
+      bishop : () => this.checkSeries(i => this.x+i, i => this.y+i)
+          || this.checkSeries(i => this.x-i, i => this.y-i)
+          || this.checkSeries(i => this.x+i, i => this.y-i)
+          || this.checkSeries(i => this.x-i, i => this.y+i),
+      rook : () => this.checkSeries(i => this.x, i => this.y+i)
+        || this.checkSeries(r, i => this.x, i => this.y-i)
+        || this.checkSeries(r, i => this.x+i, i => this.y)
+        || this.checkSeries(r, i => this.x-i, i => this.y),
+      queen : () => this.checkDetectors['rook']() || this.checkDetectors['bishop'](),
+      king : () => false
+    };
+
+    this.isCheck = this.isCheck.bind(this);
+    this.checkCell = this.checkCell.bind(this);
+    this.checkSeries = this.checkSeries.bind(this);
+  }
+
+  isCheck() {
+    if( !this.piece ) {  return false;  }
+    return this.checkDetector[this.piece.type]();
+  }
+
+  /**
+   * @return null - no check and exit from loop
+             false - no check and continue loop 
+             true - check is detected
+   */
+  checkCell(x, y) {
+    if(!valid(x) || !valid(y)) { return null; }
+    const p = this.board.get(key(x,y));
+    if(!p) { return false; }
+    return (p.white!==this.piece.white  && "king"===p.type) ? null : true;
+  }
+
+  checkSeries(nextX, nextY) {
+    for(let i=1; i<8; i++) {
+      const x = nextX(i);
+      const y = nextY(i);
+      const v = this.checkCell(x, y);
+      if( v==null ) { return false; }
+	  if(v) { return true; }
+    }
+    return false;
+  }
+}
+
+function valid(v) { return 1<=v && 8>=v; }
