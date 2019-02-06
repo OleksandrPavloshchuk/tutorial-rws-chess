15a08b36ca8a9849c1a2a8033c1bb9f4d7d582b0
Oleksandr Pavloshchuk <ap730518@gmail.com>
2019-02-06 17:17:27 +0200

    refactor

--- client/src/BoardPage.js
+++ client/src/BoardPage.js
@@ -21,34 +21,29 @@ export default class BoardPage extends Component {
   surrender = () => this.props.app.setState({askSurrender:true, acceptDeuce:false, askDeuce:false, confirmDeuce:false});
   deuce = () => this.props.app.setState({askDeuce:true, acceptDeuce:false, askSurrender:false, confirmDeuce:false});
 
-  render() {
+    render() {
 
     return (
-      <div className="container">
-        {this.props.app.isConfirm() &&
-            <QuestionModal app={this.props.app} />
-        }
-        <Navigation>
-          {this.props.app.state.message &&
-          <div className="navbar-small float-right">{this.props.app.state.message}</div>
-          }
-          {(!this.props.app.state.endGame && !this.props.app.state.myMove) &&
-          <div className="waiting-opponent navbar-small float-right"></div>
-          }
-          {(!this.props.app.state.endGame && this.props.app.state.myMove) &&
-          <div className="btn-group float-right" role="group">
-            <button className="btn btn-outline-secondary" onClick={this.deuce}
-            >Deuce</button>
-            <button className="btn btn-outline-secondary" onClick={this.surrender}
-            >Surrender</button>
-          </div>
-          }
-          {this.props.app.state.endGame &&
-          <div className="btn-group float-right" role="group">
-            <button className="btn btn-outline-secondary" onClick={this.returnToPlayerList}
-            >Exit</button>
-          </div>
-          }
+        <div className="container">
+            {this.props.app.isConfirm() &&
+                <QuestionModal app={this.props.app} />
+            }
+            <Navigation>
+              {this.props.app.state.message && <div className="navbar-small float-right">{this.props.app.state.message}</div>}
+            {this.props.app.state.endGame ? (
+                <div className="btn-group float-right" role="group">
+                    <button className="btn btn-outline-secondary" onClick={this.returnToPlayerList}>Exit</button>
+                </div>           
+            ) : (
+                this.props.app.state.myMove ? (
+                    <div className="btn-group float-right" role="group">
+                        <button className="btn btn-outline-secondary" onClick={this.deuce}>Deuce</button>
+                        <button className="btn btn-outline-secondary" onClick={this.surrender}>Surrender</button>
+                    </div>
+                ) : (
+                    <div className="waiting-opponent navbar-small float-right"></div>
+                )    
+            )}
         </Navigation>
         <div className="row">
           <Board app={this.props.app} />
