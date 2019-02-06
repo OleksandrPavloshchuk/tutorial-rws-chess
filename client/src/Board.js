15c38be72f4ec14fccc258008f6163be128bfde6
Oleksandr Pavloshchuk <ap730518@gmail.com>
2019-02-06 18:27:06 +0200

    refactor

--- client/src/Board.js
+++ client/src/Board.js
@@ -4,117 +4,77 @@ import {Motion, spring} from 'react-motion';
 
 import './assets/css/board.css';
 
-export default class Board extends Component {
-
-  render() {
-
+export default function Board(props) {
     let squares = [];
 
     let labels = ['a','b','c','d','e','f','g','h'];
-    if( !this.props.app.state.whiteMe ) {
-        labels = labels.reverse();
-    }
+    if( !props.app.state.whiteMe ) { labels = labels.reverse(); }
 
     squares.push(<LRow aKey="top" key="top" labels={labels}/>);
     for( var i=1; i<=8; i++ ) {
-        const l = this.props.app.state.whiteMe ? 9 - i : i;
-        squares.push(<Row label={l} key={i} type={i % 2} app={this.props.app} />);
+        const l = props.app.state.whiteMe ? 9 - i : i;
+        const key = "r" + i;
+        squares.push(<Row label={l} key={key} aKey={key} type={i % 2} app={props.app} />);
     }
     squares.push(<LRow aKey="bottom" key="bottom" labels={labels}/>);
 
-    return (
-        <div className="card board ">
-          <div className="card-body">
-            <table>
-            <tbody>
-            <Player name={this.props.app.state.otherPlayer} />
-            {squares}
-            <Player name={this.props.app.state.player} />
-            </tbody>
-            </table>
-          </div>
-        </div>
-    );
-  }
+    return <div className="card board "><div className="card-body"><table><tbody>
+        <Player name={props.app.state.otherPlayer} />
+        {squares}
+        <Player name={props.app.state.player} />
+    </tbody></table></div></div>;
 }
 
-class Player extends Component {
-  render() {
-    return (
-      <tr><td colSpan="10"><h5 className="card-title text-center">{this.props.name}</h5></td></tr>
-    );
-  }
+function Player(props) {
+    return <tr><td colSpan="10"><h5 className="card-title text-center">{props.name}</h5></td></tr>;
 }
 
-class LRow extends Component {
-  render() {
-    return (
-      <tr key={this.props.aKey}>
-         <td></td>
+function LRow(props) {
+    return <tr key={props.aKey}><td></td>
         {this.props.labels.map( l => <LCell text={l} key={l} aKey={l}/>)}
-        <td></td>
-      </tr>
-    );
-  }
+    <td></td></tr>;
 }
 
-class Row extends Component {
-  render() {
+function Row(props) {
     let cs = [];
 
     for(var i=1; i<=8; i++ ) {
-      const lc = this.props.app.state.whiteMe ? i  : 9 - i;
-      const key = 'c' + this.props.label + lc;
-      cs.push(
-        <Cell key={key} aKey={key} white={(i % 2) !== this.props.type} app={this.props.app} />
-      );
+        const lc = props.app.state.whiteMe ? i  : 9 - i;
+        const key = 'c' + props.label + lc;
+        cs.push(<Cell key={key} aKey={key} white={(i % 2) !== props.type} app={props.app} />);
     }
 
-    return (
-      <tr key={this.props.key}>
-        <td className="cell-label">{this.props.label}</td>
+    return <tr key={this.props.key}>
+        <td className="cell-label">{props.label}</td>
         {cs}
-        <td className="cell-label">{this.props.label}</td>
-      </tr>
-    );
-  }
+        <td className="cell-label">{props.label}</td>
+    </tr>
 }
 
-class LCell extends Component {
-  render() {
-    return (
-      <td className="cell-label" key={this.props.aKey}>{this.props.text}</td>
-    );
-  }
+function LCell(props) {
+    return <td className="cell-label" key={props.aKey}>{props.text}</td>;
 }
 
-class Cell extends Component {
+function Cell(props) {
 
-  render() {
-    const piece = this.props.app.state.board.get(this.props.aKey);
+    const piece = props.app.state.board.get(props.aKey);
     const draggable = piece 
-      && this.props.app.state.myMove 
-      && !this.props.app.state.showConversion 
-      && !this.props.app.state.endGame
-      && ((this.props.app.state.whiteMe && piece.white)
-      || (!this.props.app.state.whiteMe && !piece.white));
+        && props.app.state.myMove 
+        && !props.app.state.showConversion 
+        && !props.app.state.endGame
+        && ((props.app.state.whiteMe && piece.white)
+        || (!props.app.state.whiteMe && !piece.white));
 
-    const cellIsAvailable = this.props.app.state.board.isAvailable(this.props.aKey);
+        const cellIsAvailable = props.app.state.board.isAvailable(props.aKey);        
+        const className = 'cell-' + (props.white ? 'white' : 'black') + (cellIsAvailable ? ' cell-available' : '');
 
-    return (
-      <td className={'cell-' + (this.props.white ? 'white' : 'black') + (cellIsAvailable ? ' cell-available' : '') } key={this.props.aKey}>
-      <Droppable types={['piece']} onDrop={key => this.props.app.dropPiece(key, this.props.aKey)} >
-      {piece &&
-        <Piece white={piece.white} type={piece.type} position={this.props.aKey} draggable={draggable}
-          app={this.props.app} />
-      }
-      {!piece &&
-        <div className="cell-empty">&nbsp;</div>
-      }
-      </Droppable>
-      </td>
-    );
-  }
+        return <td className={className} key={props.aKey}>
+            <Droppable types={['piece']} onDrop={key => props.app.dropPiece(key, props.aKey)}>
+                {piece 
+                    ? <Piece white={piece.white} type={piece.type} position={props.aKey} draggable={draggable} app={props.app} />
+                    : <div className="cell-empty">&nbsp;</div>}
+            </Droppable>
+        </td>;
 }
 
 class Piece extends Component {
@@ -137,10 +97,8 @@ class Piece extends Component {
     return (
      
       this.props.draggable
-      ?
-       <Draggable type="piece" data={this.props.position} className={this.props.type + color + " piece" }
-          onDragStart={val => this.props.app.moveStart(this.props.position)}>               
-          </Draggable>
+      ? <Draggable type="piece" data={this.props.position} className={this.props.type + color + " piece" }
+          onDragStart={val => this.props.app.moveStart(this.props.position)}></Draggable>
       : <div className={this.props.type + color + " piece" }>
           <Motion defaultStyle={{opacity:1}} style={{opacity: spring(this.isCurrent() ? 1 : 0)}}>{
             style => <div style={{opacity: !this.props.app.state.myMove ? 0 : style.opacity}} className="haze"></div>
--- client/src/BoardPage.js
+++ client/src/BoardPage.js
@@ -9,50 +9,50 @@ import ConversionPanel from './ConversionPanel';
 import QuestionModal from './QuestionModal';
 
 export default class BoardPage extends Component {
-  constructor(props) {
-    super(props);
 
-    this.surrender = this.surrender.bind(this);
-    this.deuce = this.deuce.bind(this);
-    this.returnToPlayerList = this.returnToPlayerList.bind(this);
-  }
+    constructor(props) {
+        super(props);
 
-  returnToPlayerList = () => this.props.app.endGame();  
-  surrender = () => this.props.app.setState({askSurrender:true, acceptDeuce:false, askDeuce:false, confirmDeuce:false});
-  deuce = () => this.props.app.setState({askDeuce:true, acceptDeuce:false, askSurrender:false, confirmDeuce:false});
+        this.surrender = this.surrender.bind(this);
+        this.deuce = this.deuce.bind(this);
+        this.returnToPlayerList = this.returnToPlayerList.bind(this);
+    }
 
-    render() {
+    returnToPlayerList = () => this.props.app.endGame();  
+    surrender = () => this.props.app.setState({askSurrender:true, acceptDeuce:false, askDeuce:false, confirmDeuce:false});
+    deuce = () => this.props.app.setState({askDeuce:true, acceptDeuce:false, askSurrender:false, confirmDeuce:false});
 
-    return (
+    render = () => (
         <div className="container">
-            {this.props.app.isConfirm() &&
-                <QuestionModal app={this.props.app} />
-            }
+            {this.props.app.isConfirm() && <QuestionModal app={this.props.app} />}
             <Navigation>
-              {this.props.app.state.message && <div className="navbar-small float-right">{this.props.app.state.message}</div>}
-            {this.props.app.state.endGame ? (
-                <div className="btn-group float-right" role="group">
-                    <button className="btn btn-outline-secondary" onClick={this.returnToPlayerList}>Exit</button>
-                </div>           
-            ) : (
-                this.props.app.state.myMove ? (
-                    <div className="btn-group float-right" role="group">
-                        <button className="btn btn-outline-secondary" onClick={this.deuce}>Deuce</button>
-                        <button className="btn btn-outline-secondary" onClick={this.surrender}>Surrender</button>
-                    </div>
-                ) : (
-                    <div className="waiting-opponent navbar-small float-right"></div>
-                )    
-            )}
-        </Navigation>
-        <div className="row">
-          <Board app={this.props.app} />
-          {this.props.app.state.showConversion &&
-            <ConversionPanel app={this.props.app} />
-          }
-          <MoveList app={this.props.app} />
-        </div>
-      </div>
-    );
-  }
+                {this.props.app.state.message && <Message text={this.props.app.state.message}/>}
+                {this.props.app.state.endGame 
+                    ? ( <ExitButton onClick={this.returnToPlayerList} /> ) 
+                    : ( this.props.app.state.myMove 
+                        ? ( <GameActions deuce={this.deuce} surrender={this.surrender} /> ) 
+                        : ( <Waiting/> )
+                )}
+            </Navigation>
+            <div className="row">
+                <Board app={this.props.app} />
+                {this.props.app.state.showConversion && <ConversionPanel app={this.props.app} />}
+                <MoveList app={this.props.app} />
+            </div>
+        </div>);
+}
+
+function Waiting(props) { return <div className="waiting-opponent navbar-small float-right"></div>; }
+function Message(props) { return <div className="navbar-small float-right">{props.text}</div>; }
+function ExitButton(props) {
+    return <div className="btn-group float-right" role="group">
+        <button className="btn btn-outline-secondary" onClick={props.onClick}>Exit</button>
+    </div>;
+}
+
+function GameActions(props) {
+    return <div className="btn-group float-right" role="group">
+        <button className="btn btn-outline-secondary" onClick={props.deuce}>Deuce</button>
+        <button className="btn btn-outline-secondary" onClick={props.surrender}>Surrender</button>
+    </div>;
 }
