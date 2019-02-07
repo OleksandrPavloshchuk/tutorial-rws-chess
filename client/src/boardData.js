9cfc84f3b363292515ed5e57dbb1da87aa0c3b11
Oleksandr Pavloshchuk <ap730518@gmail.com>
2019-02-07 11:31:20 +0200

    refactor

--- client/src/boardData.js
+++ client/src/boardData.js
@@ -2,114 +2,104 @@ import MoveValidator from './moveValidator';
 
 export default class BoardData {
 
-  constructor(whiteMe, src) {
-    this.init = this.init.bind(this);
-    this.get = this.get.bind(this);
-    this.move = this.move.bind(this);
-    this.moveOther = this.moveOther.bind(this);
-    this.calculateAvailableCells = this.calculateAvailableCells.bind(this);
-    this.clearAvailableCells = this.clearAvailableCells.bind(this);
-    this.isAvailable = this.isAvailable.bind(this);
-    this.copyData = this.copyData.bind(this);
-    this.init = this.init.bind(this);
-    this.doMove = this.doMove.bind(this);
-    this.setNewPieceType = this.setNewPieceType.bind(this);
-    this.getPieces = this.getPieces.bind(this);
+    constructor(whiteMe, src) {
+        this.init = this.init.bind(this);
+        this.get = this.get.bind(this);
+        this.move = this.move.bind(this);
+        this.moveOther = this.moveOther.bind(this);
+        this.calculateAvailableCells = this.calculateAvailableCells.bind(this);
+        this.clearAvailableCells = this.clearAvailableCells.bind(this);
+        this.isAvailable = this.isAvailable.bind(this);
+        this.copyData = this.copyData.bind(this);
+        this.init = this.init.bind(this);
+        this.doMove = this.doMove.bind(this);
+        this.setNewPieceType = this.setNewPieceType.bind(this);
+        this.getPieces = this.getPieces.bind(this);
     
-    const y = startY(whiteMe);
+        const y = startY(whiteMe);
 
-    this.startRook1 = key(1, y);
-    this.startRook8 = key(8, y);
-    this.startKing = key(5, y);
+        this.startRook1 = key(1, y);
+        this.startRook8 = key(8, y);
+        this.startKing = key(5, y);
 
-    if(src) { this.copyData(src); } else { this.init(); }
-    this.availableCells = [];
-  }
+        if(src) { this.copyData(src); } else { this.init(); }
+        this.availableCells = [];
+    }
 
-  get = pos => this.data[pos];
+    get = pos => this.data[pos];
   
-  setNewPieceType = (pos, newType) => { if( newType ) { this.get(pos).type = newType; } };
+    setNewPieceType = (pos, newType) => { if( newType ) { this.get(pos).type = newType; } };
 
-  doMove(moveFrom, moveTo, type) {
-    this.data[moveTo] = this.data[moveFrom];
-    delete this.data[moveFrom];
-    if( moveFrom===this.startRook1 || moveTo===this.startRook1) {
-      this.rook1Moved = true;
+    doMove(moveFrom, moveTo, type) {
+        this.data[moveTo] = this.data[moveFrom];
+        delete this.data[moveFrom];
+        if( moveFrom===this.startRook1 || moveTo===this.startRook1) { this.rook1Moved = true; }
+        if( moveFrom===this.startRook8 || moveTo===this.startRook8) { this.rook8Moved = true; }
+        if( moveFrom===this.startKing || moveTo===this.startKing) { this.kingMoved = true; }
+        // Check for castling:
+        if( "king"===type) {
+            const xFrom = x(moveFrom); const xTo = x(moveTo); const yFrom = y(moveTo);
+            if( xTo-xFrom===2) { this.doMove( key(8, yFrom), key(6, yFrom), "rook");
+            } else if( xFrom-xTo===2) { this.doMove( key(1,yFrom), key(4, yFrom), "rook"); }
+        }        
     }
-    if( moveFrom===this.startRook8 || moveTo===this.startRook8) {
-      this.rook8Moved = true;
+
+    move(moveFrom, moveTo) {
+        if( moveFrom===moveTo || !this.isAvailable(moveTo)) { return false; }
+        this.doMove(moveFrom, moveTo, this.get(moveFrom).type);
+        return true;
     }
-    if( moveFrom===this.startKing || moveTo===this.startKing) {
-      this.kingMoved = true;
+
+    moveOther(moveFrom, moveTo, type) {
+        if(type) { this.data[moveFrom].type = type; }
+        this.doMove(moveFrom, moveTo, this.get(moveFrom).type );
     }
-    // Check for castling:
-    if( "king"===type) {
-      const xFrom = x(moveFrom); const xTo = x(moveTo); const yFrom = y(moveTo);
-      if( xTo-xFrom===2) { this.doMove( key(8, yFrom), key(6, yFrom), "rook");
-      } else if( xFrom-xTo===2) { this.doMove( key(1,yFrom), key(4, yFrom), "rook"); }
+
+    copyData(src) {
+        let b = {};
+        Object.keys( src.data ).forEach( key => { const v = src.data[key]; b[key] = {type: v.type, white: v.white }; });
+        this.rook1Moved = src.rook1Moved;
+        this.rook8Moved = src.rook8Moved;
+        this.kingMoved = src.kingMoved;
+        this.data = b;
     }
-  }
 
-  move(moveFrom, moveTo) {
-    if( moveFrom===moveTo || !this.isAvailable(moveTo)) { return false; }
-    this.doMove(moveFrom, moveTo, this.get(moveFrom).type);
-    return true;
-  }
+    init() {
+        this.rook1Moved = false;
+        this.rook8Moved = false;
+        this.kingMoved = false
+        let b = {};
 
-  moveOther(moveFrom, moveTo, type) {
-    if(type) {
-      this.data[moveFrom].type = type;
+        let addFigures = white => {
+            const y = startY(white);
+            b[key(1,y)] = {type: "rook", white: white};
+            b[key(2,y)] = {type: "knight", white: white};
+            b[key(3,y)] = {type: "bishop", white: white};
+            b[key(4,y)] = {type: "queen", white: white};
+            b[key(5,y)] = {type: "king", white: white};
+            b[key(6,y)] = {type: "bishop", white: white};
+            b[key(7,y)] = {type: "knight", white: white};
+            b[key(8,y)] = {type: "rook", white: white};
+        };
+        addFigures(true);
+        for( let i=1; i<=8; i++ ) { b[key(i,2)] = {type: "pawn", white: true}; }
+        addFigures(false);
+        for( let i=1; i<=8; i++ ) { b[key(i,7)] = {type: "pawn", white: false}; }
+        this.data = b;
     }
-    this.doMove(moveFrom, moveTo, this.get(moveFrom).type );
-  }
 
-  copyData(src) {
-    let b = {};
-    Object.keys( src.data ).forEach( key => { const v = src.data[key]; b[key] = {type: v.type, white: v.white }; });
-    this.rook1Moved = src.rook1Moved;
-    this.rook8Moved = src.rook8Moved;
-    this.kingMoved = src.kingMoved;
-    this.data = b;
-  }
+    clearAvailableCells = () => { this.availableCells = []; };
+    calculateAvailableCells = src => { this.availableCells = new MoveValidator(src, this).calculateAvailableCells(); };
+    isAvailable = c => this.availableCells.includes(c);
 
-  init() {
-    this.rook1Moved = false;
-    this.rook8Moved = false;
-    this.kingMoved = false
-    let b = {};
-
-    let addFigures = white => {
-      const y = startY(white);
-      b[key(1,y)] = {type: "rook", white: white};
-      b[key(2,y)] = {type: "knight", white: white};
-      b[key(3,y)] = {type: "bishop", white: white};
-      b[key(4,y)] = {type: "queen", white: white};
-      b[key(5,y)] = {type: "king", white: white};
-      b[key(6,y)] = {type: "bishop", white: white};
-      b[key(7,y)] = {type: "knight", white: white};
-      b[key(8,y)] = {type: "rook", white: white};
-    };
-    addFigures(true);
-    for( let i=1; i<=8; i++ ) { b[key(i,2)] = {type: "pawn", white: true}; }
-    addFigures(false);
-    for( let i=1; i<=8; i++ ) { b[key(i,7)] = {type: "pawn", white: false}; }
-    this.data = b;
-  }
-
-  clearAvailableCells = () => { this.availableCells = []; };
-  calculateAvailableCells = src => { this.availableCells = new MoveValidator(src, this).calculateAvailableCells(); };
-  isAvailable = c => this.availableCells.includes(c);
-
-  getPieces(my, whiteMe) {
-    let r = [];
-    Object.keys(this.data).forEach( k => {
-       const p = this.data[k];
-       if( ( my && p.white===whiteMe ) || ( !my && p.white!==whiteMe ) ) {
-       		 r.push(k);  
-       }
-    });
-    return r;
-  }
+    getPieces(my, whiteMe) {
+        let r = [];
+        Object.keys(this.data).forEach( k => {
+            const p = this.data[k];
+            if( ( my && p.white===whiteMe ) || ( !my && p.white!==whiteMe ) ) { r.push(k); }
+        });
+        return r;
+    }
 
 }
 
