a376ee686df44e4ae4cc5c7fd118596d43a2abd8
Oleksandr Pavloshchuk <ap730518@gmail.com>
2019-02-12 18:55:48 +0200

    refactor

--- client/src/PlayerListPage.js
+++ client/src/PlayerListPage.js
@@ -4,34 +4,35 @@ import Navigation from './Navigation';
 
 export default class PlayerListPage extends Component {
 
-  componentDidMount() {
-    this.props.app.mediatorClient.retrieveWaitingPlayers();
-  }
+    componentDidMount() {
+        this.props.app.mediatorClient.retrieveWaitingPlayers();
+    }
 
-  render() {
-    const items = this.props.app.state.waitingPlayers.map( name =>
-      <li
-        className="list-group-item list-group-item-action" key={name}>
-        {name}
-        <div className="btn-group float-right" role="group">
-          <button className="btn btn-outline-success"
-            onClick={ e => this.props.app.startGameMe(name, true) }
-          >Play White</button>
-          <button className="btn btn-outline-success"
-            onClick={ e => this.props.app.startGameMe(name, false) }
-          >Play Black</button>
-        </div>
-      </li>
-    )
-
-    return (
-      <div className="container">
-        <Navigation>
-          <span className="navbar-text">{this.props.app.state.player}</span>
-          <button className="btn btn-outline-secondary" onClick={this.props.app.logout}>Logout</button>
-        </Navigation>
-        <ul className="list-group">{items}</ul>
-      </div>
-    );
-  }
+    render() {
+        return <div className="container">
+            <Navigation>
+                <span className="navbar-text">{this.props.app.state.player}</span>
+                <button className="btn btn-outline-secondary" onClick={this.props.app.logout}>Logout</button>
+            </Navigation>
+            <ul className="list-group">
+                {this.props.app.state.waitingPlayers.map( name => <Player playerName={name} app={this.props.app} key={name} /> )}
+            </ul>
+        </div>;
+    }
 }
+
+function Player(props) {
+    return <li className="list-group-item list-group-item-action" key={props.playerName}>
+        {props.playerName}
+        <div className="btn-group float-right" role="group">
+            <Button text="Play White" clickHandler={e => props.app.startGameMe(props.playerName, true)} />
+            <Button text="Play Black" clickHandler={e => props.app.startGameMe(props.playerName, false)} />
+        </div>
+    </li>;
+}
+
+function Button(props) {
+    return <button className="btn btn-outline-success" onClick={props.clickHandler}>{props.text}</button>;
+}
+
+
