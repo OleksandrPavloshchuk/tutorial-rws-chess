3230d676fdb32094818fce561ac9d3ae8c56711b
Oleksandr Pavloshchuk <ap730518@gmail.com>
2019-02-19 13:52:29 +0200

    adaptive piece size: start

--- client/src/services/config.js
+++ client/src/services/config.js
@@ -1,6 +1,6 @@
 export default class Configuration {
   constructor() {
-    //this.webSocketUrl = "ws://localhost:3016/ws";
-    this.webSocketUrl = "ws://192.168.1.9:3016/ws";
+    this.webSocketUrl = "ws://localhost:3016/ws";
+    //this.webSocketUrl = "ws://192.168.1.9:3016/ws";
   }
 }
