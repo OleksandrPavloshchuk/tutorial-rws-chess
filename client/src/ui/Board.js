daa9c2ca4005b9cef7b29ea66b92571be96d67ab
Oleksandr Pavloshchuk <ap730518@gmail.com>
2019-02-21 12:05:41 +0200

    refatoring

--- client/src/ui/Board.js
+++ client/src/ui/Board.js
@@ -26,35 +26,27 @@ export default function Board(props) {
     </tbody></table></div></div>;
 }
 
-function Player(props) {
-    return <tr><td colSpan="10"><h5 className="card-title text-center">{props.name}</h5></td></tr>;
-}
-
-function LRow(props) {
-    return <tr key={props.aKey}><td></td>
-        {props.labels.map( l => <LCell text={l} key={l} aKey={l}/>)}
-    <td></td></tr>;
-}
+var Player = props => <tr><td colSpan="10">
+    <h5 className="card-title text-center">{props.name}</h5></td></tr>;
+    
+var LRow = props => <tr key={props.aKey}><td></td>
+    {props.labels.map( l => <LCell text={l} key={l} aKey={l}/>)}<td></td></tr>;
+    
+var LCell = props => <td className="cell-label" key={props.aKey}>{props.text}</td>;    
 
 function Row(props) {
-    let cs = [];
+    let cells = [];
 
     for(var i=1; i<=8; i++ ) {
         const lc = props.app.state.whiteMe ? i  : 9 - i;
         const key = 'c' + props.label + lc;
-        cs.push(<Cell key={key} aKey={key} white={(i % 2) !== props.type} app={props.app} />);
+        cells.push(<Cell key={key} aKey={key} white={(i % 2) !== props.type} app={props.app} />);
     }
-
-    return <tr key={props.aKey} >
-        <td className="cell-label">{props.label}</td>
-        {cs}
-        <td className="cell-label">{props.label}</td>
-    </tr>
+    
+    const rowNum = <td className="cell-label">{props.label}</td>;
+    return <tr key={props.aKey}>{rowNum}{cells}{rowNum}</tr>;
 }
 
-function LCell(props) {
-    return <td className="cell-label" key={props.aKey}>{props.text}</td>;
-}
 
 class Cell extends Component {
 
@@ -80,22 +72,21 @@ class Cell extends Component {
         const cellIsAvailable = this.props.app.state.board.isAvailable(this.props.aKey);        
         const className = 'board-cell cell-' + (this.props.white ? 'white' : 'black') + (cellIsAvailable ? ' cell-available' : '');
 
-    var renderDroppable = () => <Droppable types={['piece']} onDrop={key => this.props.app.dropPiece(key, this.props.aKey)} style={this.getStyle()}>
+        var renderDroppable = () => <Droppable types={['piece']} onDrop={key => this.props.app.dropPiece(key, this.props.aKey)} style={this.getStyle()}>
                 {piece 
                     ? <Piece white={piece.white} type={piece.type} position={this.props.aKey} draggable={draggable} app={this.props.app} />
                     : <div className="cell-empty">&nbsp;</div>}
             </Droppable>;
 
-      var renderClickable = () => <div onClick={event => this.props.app.dropPiece({piece:this.props.app.state.moveFrom}, this.props.aKey)} style={this.getStyle()}>
+        var renderClickable = () => <div onClick={event => 
+                this.props.app.dropPiece({piece:this.props.app.state.moveFrom}, this.props.aKey)} style={this.getStyle()}>
                 {piece 
                     ? <Piece white={piece.white} type={piece.type} position={this.props.aKey} draggable={draggable} app={this.props.app} />
                     : <div className="cell-empty">&nbsp;</div>}
             </div>;
 
         return <td className={className} key={this.props.aKey}>
-            {this.props.app.state.useDragAndDrop  
-			? renderDroppable()
-            : renderClickable()}
+            {this.props.app.state.useDragAndDrop ? renderDroppable() : renderClickable()}
         </td>;
     }
 }
@@ -115,30 +106,23 @@ class Piece extends Component {
         ? (this.props.position===this.props.app.state.moveOtherTo)
         : false;
         
-    renderClickable() { return <div className="piece" style={this.getStyle()}
+    renderClickable = () => <div className="piece" style={this.getStyle()}
 		onClick={event => this.props.app.moveStart(this.props.position)}></div>;
-    }
 
-	renderDraggable() { return <Draggable type="piece" data={this.props.position} 
+	renderDraggable = () => <Draggable type="piece" data={this.props.position} 
 	    className="piece" style={this.getStyle()}
-    	onDragStart={val => this.props.app.moveStart(this.props.position)}></Draggable>;		
-    }
+    	onDragStart={val => this.props.app.moveStart(this.props.position)}></Draggable>;
 
-	renderCommon() { return <div className="piece" style={this.getStyle()} >
+	renderCommon = () => <div className="piece" style={this.getStyle()} >
     		<Motion defaultStyle={{opacity:1}} style={{opacity: spring(this.isCurrent() ? 1 : 0)}}>
         		{style => <div style={{opacity: !this.props.app.state.myMove ? 0 : style.opacity}} className="haze"></div>}
         	</Motion>
     	</div>;
-    }
     
     getStyle = () => getPieceStyle(this.props.app.state.cellSize, this.props.type, this.props.white);            
 
-    render() {
-        return this.props.draggable
-            ? (this.props.app.state.useDragAndDrop 
-		        ? this.renderDraggable()
-                : this.renderClickable())
+    render = () => this.props.draggable
+            ? (this.props.app.state.useDragAndDrop ? this.renderDraggable() : this.renderClickable())
             : this.renderCommon();
-    }
 
 }
