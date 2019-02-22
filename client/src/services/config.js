e7341be3782dffabf499509f4761ec996b3ebd9f
Oleksandr Pavloshchuk <ap730518@gmail.com>
2019-02-22 11:41:25 +0200

    localhost - for tests

--- client/src/services/config.js
+++ client/src/services/config.js
@@ -1,5 +1,6 @@
 export default class Configuration {
   constructor() {
+    //this.webSocketUrl = "ws://10.91.161.109:3016/ws";
     this.webSocketUrl = "ws://localhost:3016/ws";
     //this.webSocketUrl = "ws://192.168.1.9:3016/ws";
   }
